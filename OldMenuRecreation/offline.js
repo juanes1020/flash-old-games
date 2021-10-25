b626779ec6d2468dab4be4eb0e2f9f02a1d9d01a
Juaniesteban1020 <78309668+juaniesteban1020@users.noreply.github.com>
2021-10-25 16:00:19 -0300

    Add files via upload

---
+++ OldMenuRecreation/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1635187944,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/bg-sheet1.png",
+		"images/buttons-sheet0.png",
+		"images/buttons-sheet1.png",
+		"images/buttons-sheet2.png",
+		"images/logoyellow-sheet0.png",
+		"images/logoyellow-sheet1.png",
+		"images/spritefont.png",
+		"images/logoloading-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/telaraña-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"penumbra.png"
+	]
+}
